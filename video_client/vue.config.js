335b3907b0977c640269bf08a3548fa81aafe312
suchang <1103232282@qq.com>
2019-07-25 20:37:55 +0800

    整理项目

--- video_client/vue.config.js
+++ video_client/vue.config.js
@@ -39,12 +39,6 @@ module.exports = {
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: { '^/api': '' },
       },
-      '/data_report': {
-        target: 'http://localhost:5000/', // 接口的域名  //http://47.92.110.74:8085/
-        secure: false, // 如果是https接口，需要配置这个参数
-        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        pathRewrite: { '^/data_report': '' }
-      },
       [process.env.VUE_APP_BASE_API]: {
         target: `http://localhost:${port}/mock`,
         changeOrigin: true,
