2ca83c7d6e8d3ad940448eaa8d73a9e861096831
suchang <1103232282@qq.com>
2019-08-02 11:15:07 +0800

    静态文件

--- video_sdk/rpc.py
+++ video_sdk/rpc.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
 from flask_jsonrpc import JSONRPC
+import os
 
 app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/rpc')
@@ -69,3 +70,13 @@ def de_tbumbnail_by_path(path, outpath):
     :return:
     """
     pass
+
+@app.route('/api/read_file', methods=['GET'])
+def read_file():
+    path=request.args.get('path')
+    if not path or not os.path.isabs(path):
+        return jsonify(code=404, msg='没有对应的文件'), 200
+    return send_file(path)
+
+if __name__ == "__main__":
+    app.run(port=5005)
