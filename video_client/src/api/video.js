54a41b8139c5f408741dc2b0728fe33703f10272
suchang <1103232282@qq.com>
2019-08-05 13:52:29 +0800

    视频下载播放

--- video_client/src/api/video.js
+++ video_client/src/api/video.js
@@ -1,9 +1,20 @@
+import {ipcMain} from 'electron'
 import axios from '@/utils/request'
+import httpAdapter from '@/utils/http'
 
+const BASE_URL = 'http://47.104.148.221:8082'
 export function videoDownload(videoId) {
     return axios({
-      url: '/api/videos/download/:id',
+      url: `${BASE_URL}/videos/download/${videoId}`,
       method: 'get',
-      data: {id: videoId}
+      responseType: 'stream',
+      adapter:httpAdapter
     })
   }
+
+  export function queryVideos (args)  {
+    return axios.post('/api/videos',
+      {"_method": "GET", "_args": args})
+  }
+
+
--- video_client/src/rpc/index.js
+++ video_client/src/rpc/index.js
@@ -25,22 +25,15 @@ Rpc.enWaterMarkByPath = function(path, outpath) {
     })
 }
 
-Rpc.redaLocalFile = function(path){
+Rpc.readLocalFile = function(path){
     //console.log(`${BASE_URL}/api/read_file`)
     return axios.get(`${BASE_URL}/rpc/read_file`, { params: {'path': path} })
 }
 
-Rpc.downloadFile = function(path){
-    // return axios.get('http://47.104.148.221:8082/videos/download/'+ path)
-    return axios.get('http://47.104.148.221:8082/videos/download/'+ path)
+Rpc.readLocalUrl = function(path){
+  return `file://${path}`
 }
 
-Rpc.readVideoList = function(){
-    return axios.post('http://47.104.148.221:8082/videos', {"_method": "GET", "_args": {}})
-    // .then( (response)=> {
-    //   console.log(response);
-    //   this.tableData = response.data.videos;
-    // });
-}
+
 
 export default Rpc;
