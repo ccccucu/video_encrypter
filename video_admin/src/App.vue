ae9259d544881f4155624ab483c3c1d11fc9d858
吴坤泽 <894607015@qq.com>
2019-08-20 22:29:58 +0800

    前端：用户关闭网页自动消除cookie

--- video_admin/src/App.vue
+++ video_admin/src/App.vue
@@ -5,7 +5,21 @@
 </template>
 
 <script>
+  import { removeToken } from '@/utils/auth'
 export default {
-  name: 'App'
+
+  name: 'App',
+  mounted(){
+    // 关闭浏览器窗口的时候清空浏览器缓存在localStorage的数据
+    window.onbeforeunload = function (e) {
+      var storage = window.localStorage;
+      storage.clear()
+      removeToken()
+    }
+  },
+  beforeDestroy() {
+    removeToken()
+  }
+
 }
 </script>
